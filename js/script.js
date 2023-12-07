a09cf586430da8b3cbf35a9af5a07f2b7831667b
AST <dias.almagambetov01@gmail.com>
2023-12-07 20:34:56 +0600

    First version

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log(2137);
+let xmpLink = 2332;
+const RT = 1500;
+const COLOR_RED = "#F00";
+const rat = {
+    name: "Recrent",
+    age: 32
+};
+console.log(rat);
+const rt = "b";
+rat.xt = "Shit";
+rat.rt = "231";
+rat["xtx"] = "Shix";
+console.log(rat["xt"]);
+console.log(rat.xtx);
+console.log(rat.rt);
+const ans = [];
+ans[0] = prompt("Youe Name?!", "");
+ans[1] = prompt("Youe Surname?!", "");
+ans[2] = prompt("Youe Age?!", "");
+document.write(ans);
+alert(`Hello! ${rat.name}`)
