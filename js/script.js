438a98932e6bb5fa0ec074bc0d3950cf215f38e0
almagamdias <dias.almagambetov01@gmail.com>
2023-12-09 17:14:59 +0600

    Modified JS

--- js/script.js
+++ js/script.js
@@ -6,10 +6,21 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-const a = prompt('One of the last watched films?'),
-      b = prompt('How much rate it?'),
-      c = prompt('One of the last watched films?'),
-      d = prompt('How much rate it?');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++) {
+    const a = prompt('One of the last watched films?'), b = prompt('How much rate it?');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('ERROR!!'); i--;
+    }
+}
+if (personalMovieDB.count < 10)
+    console.log("IT is TOOOOO LOW!!!");
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
+    console.log("IT is NORMAL");
+else if (personalMovieDB.count >= 30)
+    console.log("IT is TOOOOO HIGH!!!");
+else
+    console.log("ERRRRRORRRR!!!!");
 console.log(personalMovieDB);
