079b308b2b0354e254debd9e22e0347861fe8b31
almagamdias <dias.almagambetov01@gmail.com>
2023-12-09 18:31:34 +0600

    Modified JS2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const numberOfFilms = +prompt('How many films have watched?');
+let numberOfFilms;
+function start() {
+    numberOfFilms = prompt('How many films have watched?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('How many films have watched?', '');
+    }
+}
+start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -6,21 +13,42 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-for(let i = 0; i < 2; i++) {
-    const a = prompt('One of the last watched films?'), b = prompt('How much rate it?');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
-    } else {
-        console.log('ERROR!!'); i--;
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('One of the last watched films?'), b = prompt('How much rate it?');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        } else {
+            console.log('ERROR!!'); i--;
+        }
     }
 }
-if (personalMovieDB.count < 10)
-    console.log("IT is TOOOOO LOW!!!");
-else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
-    console.log("IT is NORMAL");
-else if (personalMovieDB.count >= 30)
-    console.log("IT is TOOOOO HIGH!!!");
-else
-    console.log("ERRRRRORRRR!!!!");
-console.log(personalMovieDB);
+rememberMyFilms();
+function detectLevel() {
+    if (personalMovieDB.count < 10)
+        console.log("IT is TOOOOO LOW!!!");
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
+        console.log("IT is NORMAL");
+    else if (personalMovieDB.count >= 30)
+        console.log("IT is TOOOOO HIGH!!!");
+    else
+        console.log("ERRRRRORRRR!!!!");
+    console.log(personalMovieDB);
+}
+detectLevel();
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+//showMyDB(personalMovieDB.private);
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Your favourite genre in number ${i}`);
+    }
+}
+writeYourGenres();
+function getS() {
+    console.log("CLICKED!!");
+}
